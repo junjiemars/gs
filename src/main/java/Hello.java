77794b3b6893426a2f06c4c6e4c5ce6ec62cfe61
Junjie Mars <junjiemars@gmail.com>
2014-09-29 14:06:57 +0800

    the first gradle java project

---
+++ src/main/java/Hello.java
@@ -0,0 +1,10 @@
+/**
+ * Created by junjie on 9/29/14.
+ */
+import static java.lang.System.out;
+
+public class Hello {
+    public static void main(String[] args) {
+        out.println("Hello");
+    }
+}
