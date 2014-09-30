e1b1470ee84c8afbd0dcb64ae19768b937b9acbc
Junjie Mars <junjiemars@gmail.com>
2014-09-30 09:42:41 +0800

    translate ant-ivy to gradle

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,10 +1,17 @@
 /**
  * Created by junjie on 9/29/14.
  */
+import com.google.gson.Gson;
+
 import static java.lang.System.out;
 
 public class Hello {
     public static void main(String[] args) {
         out.println("Hello");
+
+        Gson gson = new Gson();
+        String j = gson.toJson("{ name: John, age: 28 }");
+        out.println(j);
+
     }
 }
