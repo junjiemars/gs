9a560bd2e4ab476bd5e486f6b37a1be8a55e70a0
Junjie Mars <junjiemars@gmail.com>
2014-11-25 14:53:17 +0800

    Oh, the split method of regex

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -3,15 +3,19 @@
  */
 import com.google.gson.Gson;
 
+import java.util.regex.Pattern;
+
 import static java.lang.System.out;
 
 public class Hello {
     public static void main(String[] args) {
-        out.println("Hello");
-
-        Gson gson = new Gson();
-        String j = gson.toJson("{ name: John, age: 28 }");
-        out.println(j);
-
+        final String s = "新夜话无眠：4G^10.0，， ，  漫游^20.1。闲时^19.2|省钱^1.5";
+        final Pattern p = Pattern.compile("\\s*[ \\u3002:\\uff1a,\\uff0c|]\\s*");
+        final String[] a = p.split(s);
+        for (String i : a) {
+            out.println(i);
+        }
     }
+
+    public static final String S_HELLO="Hello,world";
 }
